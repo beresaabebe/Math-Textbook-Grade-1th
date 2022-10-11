94eeca18c4d4f9e613e59f5120e7b60f47fe6287
Becky <roobaanuuf@gmail.com>
2022-10-11 17:48:21 +0300

    save all

--- app/src/main/java/com/beckytech/mathtextbookgrade11th/contents/SubTitleContents.java
+++ app/src/main/java/com/beckytech/mathtextbookgrade11th/contents/SubTitleContents.java
@@ -2,11 +2,17 @@ package com.beckytech.mathtextbookgrade11th.contents;
 
 public class SubTitleContents {
     public String[] subTitle = {
+            "Intro",
+            "Unit 1",
+            "Unit 2",
+            "Unit 3",
+            "Unit 4",
+            "Unit 5",
+            "Unit 6",
+            "Unit 7",
+            "Unit 8",
+            "Unit 9",
             "Unit 10",
-            "Unit 11",
-            "Unit 12",
-            "Unit 13",
-            "Unit 14",
-            "Unit 15"
+            "Unit 11"
     };
 }
--- app/src/main/java/com/beckytech/mathtextbookgrade11th/contents/TitleContents.java
+++ app/src/main/java/com/beckytech/mathtextbookgrade11th/contents/TitleContents.java
@@ -2,11 +2,17 @@ package com.beckytech.mathtextbookgrade11th.contents;
 
 public class TitleContents {
     public String[] title = {
-            "Introduction to Limit and Continuity",
-            "Sequence & Series",
-            "Introduction to Differential & calculus",
-            "Application of Differential & Calculus",
-            "Integral Calculus",
-            "Three dimensional Geometry"
+            "Cover and Contents",
+            "Further on Relation and Functions",
+            "Rational Expressions and Rational Functions",
+            "Coordinate Geometry",
+            "Mathematical Reasoning",
+            "Statistics and Probability",
+            "Matrices and determinants",
+            "The set of complex Number",
+            "Vectors and Transformations of the plane",
+            "Further on trigonometric Function",
+            "Introduction to Linear Programming",
+            "Mathematical Applications in Business"
     };
 }
